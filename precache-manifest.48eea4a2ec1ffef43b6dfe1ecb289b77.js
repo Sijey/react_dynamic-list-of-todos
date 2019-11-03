fcb75f4ec0e91888ed177458f5a51c2a25bf939f
Sijey <Popov040391@gmail.com>
2019-11-03 12:15:37 +0200

    Updates

---
+++ precache-manifest.48eea4a2ec1ffef43b6dfe1ecb289b77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa735381cc4f4a09d0db227ca51de31",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "738dea8aabf4a5d1cd0a",
+    "url": "/react_dynamic-list-of-todos/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "443f61b8af495a964d11",
+    "url": "/react_dynamic-list-of-todos/static/js/2.7968af43.chunk.js"
+  },
+  {
+    "revision": "738dea8aabf4a5d1cd0a",
+    "url": "/react_dynamic-list-of-todos/static/js/main.26c19942.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
